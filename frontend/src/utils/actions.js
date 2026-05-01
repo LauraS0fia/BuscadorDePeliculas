ac402820ac9235892401877fc7b36eb68d6a2c6b
laura <lauritrujillo123@gmail.com>
2026-04-30 22:16:35 -0500

    Fin prueba API

--- frontend/src/utils/actions.js
+++ frontend/src/utils/actions.js
@@ -1,12 +1,15 @@
 import axios from 'axios'
 
-export const getFirstMovies = async (filtro = '') => {
+export const getFirstMovies = async (filtro = 'star wars') => {
     try {
-        const { VITE_URL_API } = import.meta.env;
-        const response = await axios.get(VITE_URL_API);
+        const { VITE_URL_API, VITE_API_KEY } = import.meta.env;
+        const filters = `s=${filtro}&page=10`
+
+        const query = `${VITE_URL_API}${filters}&${VITE_API_KEY}`
+        const response = await axios.get(query);
         return response.data;
     } catch (error) {
         console.error(error);
         throw error;
     }
-}
+};
