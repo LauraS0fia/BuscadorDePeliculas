1b68b740bb8d0157405517fb11f4a7889dc30ea1
laura <lauritrujillo123@gmail.com>
2026-04-30 23:17:13 -0500

    inicio frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,45 @@
 import { useEffect, useState } from 'react'
-import './App.css'
 import { getFirstMovies } from './utils/actions'
+import { ThemeProvider } from 'styled-components';
+import { lightTheme } from './theme/theme'
+import { Title } from './assets/styled-component/Title'
+import { CardMovie } from './assets/styled-component/CardMovie'
 
 function App() {
+  const [movies, setMovies] = useState([]);
   useEffect(() => {
-
-    const fetchData = async () => {
-      try {
-        const movies = await getFirstMovies();
-        console.log(movies);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchData();
-  }, []);
+    getMovie()
+  }, [])
+  const getMovie = async () => {
+    try {
+      const response = await getFirstMovies()
+      const { Search } = response
+      setMovies(Search)
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
   return (
-    <>
-      <h1>Hola</h1>
-    </>
+    <ThemeProvider theme={lightTheme}>
+      <Title>Hola</Title>
+      {
+        movies.map((movie) => {
+          const { Title, Poster, Type, Year, imdbID } = movie
+
+          return (
+            <CardMovie
+              key={imdbID}
+              title={Title}
+              poster={Poster}
+              type={Type}
+              year={Year}
+            />
+          )
+        })
+      }
+    </ThemeProvider>
   )
-}
+};
 
 export default App
