a05d1be4d40b0ed7fa104b78d99493d7439866fe
Sofia Pedraza <157745822+LauraS0fia@users.noreply.github.com>
2026-05-05 11:42:47 -0500

    Refactor por cambio de endpoint

--- frontend/src/utils/actions.js
+++ frontend/src/utils/actions.js
@@ -1,15 +1,21 @@
-import axios from 'axios'
+import axios from 'axios';
 
-export const getFirstMovies = async (filtro = 'star wars') => {
-    try {
-        const { VITE_URL_API, VITE_API_KEY } = import.meta.env;
-        const filters = `s=${filtro}&page=10`
+export const getFirstMovies = async (query = '') => {
+  try {
+    const { VITE_URL_API, VITE_API_KEY } = import.meta.env;
+    const endpoint = query ? '/search/movie' : '/discover/movie';
+    const params = new URLSearchParams({
+      api_key: VITE_API_KEY,
+      language: 'es-ES',
+      page: '1',
+      ...(query ? { query } : { sort_by: 'popularity.desc' }),
+    });
 
-        const query = `${VITE_URL_API}${filters}&${VITE_API_KEY}`
-        const response = await axios.get(query);
-        return response.data;
-    } catch (error) {
-        console.error(error);
-        throw error;
-    }
+    const url = `${VITE_URL_API}${endpoint}?${params.toString()}`;
+    const response = await axios.get(url);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 };
